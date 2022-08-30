9102da2cc9b73160bf0f62638c1f87329337d77b
Anton Manuylov <killsonic4@gmail.com>
2022-08-31 01:44:46 +0300

    4th task added

---
+++ Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number_0 = int.Parse(Console.ReadLine());
+int x = 2;
+    if (number_0 >= x)
+    {
+        while (x <= number_0)
+        { 
+            Console.WriteLine(x);
+            x += 2;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введенное число меньше 2");
+    }
