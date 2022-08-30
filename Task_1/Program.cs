9b731deed2c47e7d07614436a233cbf0202d036b
Anton Manuylov <killsonic4@gmail.com>
2022-08-31 01:05:43 +0300

    1st task added

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number_0 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number_1 = int.Parse(Console.ReadLine());
+if (number_0 == number_1)
+    {
+        Console.WriteLine("Числа равны");
+    }
+else 
+    {
+        if (number_0 > number_1)
+            {
+                Console.WriteLine($"Число {number_0} больше числа {number_1}");
+            }
+        else
+            {
+                Console.WriteLine($"Число {number_1} больше числа {number_0}");
+            }
+    }
